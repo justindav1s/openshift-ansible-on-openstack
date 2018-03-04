5fb6030661fc1a3f90c3106cc3035741ddcc0a99
Justin Davis <justinndavis@gmail.com>
2018-03-04 14:36:15 +0000

    ansible updates

--- bin/build_ocp_infra.sh
+++ bin/build_ocp_infra.sh
@@ -2,4 +2,8 @@
 
 rm -rf ../ansible/*.retry
 
-ansible-playbook  -i ../ansible/inventory ../ansible/servers_from_snapshots.yml
+nohup ansible-playbook  -i ../ansible/inventory ../ansible/servers_from_snapshots.yml  > servers_from_snapshots.log 2>&1 &
+
+sleep 5
+
+tail -f servers_from_snapshots.log
