11afcae16207b813cb924b94259f9016b1c58b9f
Justin Davis <justinndavis@gmail.com>
2018-03-10 11:46:50 +0000

    ansible updates

--- bin/build_ocp_infra.sh
+++ bin/build_ocp_infra.sh
@@ -2,8 +2,4 @@
 
 rm -rf ../ansible/*.retry
 
-nohup ansible-playbook  -i ../ansible/inventory ../ansible/build_ocp_infra.yml  > build_ocp_infra.log 2>&1 &
-
-sleep 5
-
-tail -f build_ocp_infra.log
+nohup ansible-playbook  -i ../ansible/inventory ../ansible/build_ocp_infra.yml
