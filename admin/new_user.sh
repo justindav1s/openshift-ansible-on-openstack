8cd49ecb472efe9a90706e93ef592453ec7a6b1d
Justin Davis <justinndavis@gmail.com>
2018-03-11 14:30:08 +0000

    ansible updates

--- admin/new_user.sh
+++ admin/new_user.sh
@@ -8,6 +8,23 @@ oc login https://master1.swlon.local:8443 -u justin
 oc delete user ${USER}
 oc delete identity htpasswd_auth:${USER}
 oc delete project ${PROJECT}
+oc delete project $PROJECT
+
+oc process -f pinned-project-request-template.yaml \
+    -p PROJECT_NAME=${PROJECT} \
+    -p PROJECT_ADMIN_USER=${USER} \
+    -p PROJECT_REQUESTING_USER=${USER} \
+    -p NODE_SELECTOR="${USER}=true" | oc create -f - 2> /dev/null
+
+while [ $? \> 0 ]; do
+    sleep 1
+    printf "."
+    oc process -f pinned-project-request-template.yaml \
+    -p PROJECT_NAME=${PROJECT} \
+    -p PROJECT_ADMIN_USER=${USER} \
+    -p PROJECT_REQUESTING_USER=${USER} \
+    -p NODE_SELECTOR="${USER}=true" | oc create -f - 2> /dev/null
+done
 
 oc process -f user-template.yaml -p USERNAME=${USER} | oc create -f -
 
