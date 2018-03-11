99157c96aad93fbf49c82f72decd7d06cbe108a7
Justin Davis <justinndavis@gmail.com>
2018-03-11 13:59:51 +0000

    ansible updates

--- admin/new_user.sh
+++ admin/new_user.sh
@@ -7,3 +7,4 @@ oc delete user client1
 oc login https://master1.swlon.local:8443 -u justin
 
 oc process -f user-template.yaml -p USERNAME=client1 | oc create -f -
+oc process -f role-binding-template.yaml -v USERNAME=client1 | oc create --namespace=client1_project -f -
