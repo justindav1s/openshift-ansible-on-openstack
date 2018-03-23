2b5d6009b3eece69260a660a86246f098236e9d6
Justin Davis <justinndavis@gmail.com>
2018-03-23 20:07:41 +0000

    playbook updates

--- bin/base_server_setup.sh
+++ bin/base_server_setup.sh
@@ -2,4 +2,6 @@
 
 rm -rf ../ansible/*.retry
 
+ansible-playbook  -vv -i ../ansible/inventory ../ansible/build_base_server.yml
+sleep 60
 ansible-playbook  -vv -i ../ansible/inventory ../ansible/base_server_config.yml
