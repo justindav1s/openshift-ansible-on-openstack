ad4f46e6b1e1698c7e184e639f359fdd3539b59d
Justin Davis <justinndavis@gmail.com>
2018-05-14 15:08:59 +0100

    nvidia support

---
+++ nvidia/deploy_caffe2.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+oc login https://192.168.0.20:8443 -u justin
+
+oc project nvidia
+
+oc create serviceaccount -n nvidia privilegeduser
+oc adm policy add-scc-to-user privileged -n nvidia -z privilegeduser
+
+oc create -f caffe2.yaml
+
