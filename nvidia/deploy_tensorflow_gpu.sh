ea0f6fd0f1a794b0a9d715456f0b9e4fa3bb68cd
Justin Davis <justinndavis@gmail.com>
2018-10-06 16:12:02 +0100

    openstack/nvidia

--- nvidia/deploy_tensorflow_gpu.sh
+++ nvidia/deploy_tensorflow_gpu.sh
@@ -1,14 +1,15 @@
 #/bin/bash
 oc login https://192.168.0.19:8443 -u justin
 
-oc project nvidia
+PROJECT=tensorflow
 
-oc delete all -l app=tensorflow -n nvidia
-oc delete all -l app=tensorflow-gpu -n nvidia
-oc delete serviceaccount tensorflowuser -n nvidia
+oc project $PROJECT
 
-oc create serviceaccount tensorflowuser -n nvidia
-oc adm policy add-scc-to-user privileged -z tensorflowuser -n nvidia
+oc delete all -l app=tensorflow
+oc delete all -l app=tensorflow-gpu
+oc delete serviceaccount tensorflowuser
+oc create serviceaccount tensorflowuser
+oc adm policy add-scc-to-user privileged -z tensorflowuser
 
 oc new-app -f tensorflow_gpu.yaml
-
+oc new-app -f tensorflow.yaml
