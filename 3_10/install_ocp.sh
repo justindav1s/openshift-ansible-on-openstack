f920038d2d389f344a1aa8f2a44235af11f8932f
Justin Davis <justinndavis@gmail.com>
2018-10-09 10:12:50 +0100

    openstack/nvidia

--- 3_10/install_ocp.sh
+++ 3_10/install_ocp.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 
-ansible-playbook -i hosts.3.10 \
-   /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml
+nohup ansible-playbook -i hosts.3.10 \
+   /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml \
+   > install.log &
+
+sleep 5
+
+tail -f install.log
--- 3_10/prerequisites.sh
+++ 3_10/prerequisites.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 
-ansible-playbook -i hosts.3.10 \
-   /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml
+nohup ansible-playbook -i hosts.3.10 \
+   /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml \
+   > prereqs.log &
+
+sleep 5
+
+tail -f prereqs.log
