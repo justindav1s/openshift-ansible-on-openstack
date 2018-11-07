0883d563adf80b7b732dd4aa2fe92bf74c06d452
Justin Davis <justinndavis@gmail.com>
2018-11-07 17:44:54 +0000

    nfs

--- 3_11/install_ocp.sh
+++ 3_11/install_ocp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-nohup ansible-playbook -i hosts.3.11 \
+nohup ansible-playbook -i hosts.3.11.singleinstance \
    /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml \
    -e openshift_logging_install_logging=False \
    -e openshift_metrics_install_metrics=False \
