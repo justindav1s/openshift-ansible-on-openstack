5d134c705c88f42a894589f2d98d0089b00c2d60
Justin Davis <justinndavis@gmail.com>
2019-01-22 15:09:56 +0000

    pv create script updates

--- 3_11/host_pvs.sh
+++ 3_11/host_pvs.sh
@@ -1,22 +1,22 @@
 #/bin/bash
 oc login https://ocp.datr.eu:8443 -u justin
 
-ENV=app
+ENV=`hostname`
 HOST_ROOT=/kube_volumes
 
 PV_STUB=${ENV}-pv000
 
 sudo mkdir $HOST_ROOT
 
-for i in {1..10}; do
+for i in {1..20}; do
 	PV_NAME=$PV_STUB$i
 	echo Setting up $PV_NAME
 	oc delete pv $PV_NAME
 	sudo rm -rf $HOST_ROOT/$PV_NAME
 	sudo mkdir $HOST_ROOT/$PV_NAME
-	cat ${ENV}-pv-template.yml | sed s/XXXX/$PV_NAME/g > app-$PV_NAME.yml
-	oc create -f app-$PV_NAME.yml
-	rm -rf app-$PV_NAME.yml
+	cat pv-template.yml | sed s/XXXX/$PV_NAME/g > tmp-$PV_NAME.yml
+	oc create -f tmp-$PV_NAME.yml
+	rm -rf tmp-$PV_NAME.yml
 done
 
 sudo chmod -R 777 $HOST_ROOT
