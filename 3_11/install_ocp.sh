1ea16609b002272e2899c50b04201548dd8fd4e4
Justin Davis <justinndavis@gmail.com>
2019-06-22 16:51:58 +0100

    aws stuff

--- 3_11/install_ocp.sh
+++ 3_11/install_ocp.sh
@@ -4,7 +4,7 @@
 nohup ansible-playbook -vvv -i hosts.3.11.singleinstance.aws \
    /usr/share/ansible/openshift-ansible/playbooks/deploy_cluster.yml \
    -e openshift_logging_install_logging=False \
-   -e openshift_metrics_install_metrics=True \
+   -e openshift_metrics_install_metrics=False \
    > install.log &
 
 sleep 5
